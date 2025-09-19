a3445d597234ef94520b18c3c5f5382dd3698237
okonys <yoonsg1007@gmail.com>
2025-09-19 15:59:19 +0900

    기본 프로젝트

---
+++ src/main/java/com/example/dream_compass/DreamCompassApplication.java
@@ -0,0 +1,13 @@
+package com.example.dream_compass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamCompassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamCompassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dream_compass/DreamCompassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dream_compass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamCompassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
